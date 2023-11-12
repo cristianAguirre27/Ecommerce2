871d4b6602e8cf85c9f0eb78b0d92699c2a2e2a9
Cristian Aguirre <cristian10323@gmail.com>
2023-11-12 00:09:08 -0500

    se agrega cors en app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require ('express');
 const morgan = require ('morgan');
 const cookieParser = require('cookie-parser');
+const cors = require('cors')
 
 const authRoutes = require('./routes/auth.routes.js')
 const itemRoutes = require('./routes/item.routes.js')
 
 const server = express()
 
+server.use(cors());
 server.use(morgan('dev'));
 server.use(express.json())
 server.use(cookieParser());
