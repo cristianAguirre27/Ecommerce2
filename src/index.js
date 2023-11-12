3b09e11d7c291d0a852137fddcfbc5e31ada387c
Cristian Aguirre <cristian10323@gmail.com>
2023-11-11 21:29:58 -0500

    se corrige arranque

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const server = require ('./app.js');
 
-server.listen(3000);
-console.log('Server on Port ', 3000);
+const puerto = process.env.PORT || 3000
+server.listen(puerto,()=>{
+    console.log(`Servidor ejecutandose ${puerto}`)
+})
